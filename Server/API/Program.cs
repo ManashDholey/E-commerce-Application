5e8cc322a133776f6d8af46c05553c53cf48828f
Manash <Manash@DESKTOP-H7BTIBJ>
2023-11-19 07:44:51 +0530

    Commit for add new tables

--- Server/API/Program.cs
+++ Server/API/Program.cs
@@ -46,6 +46,23 @@ var summaries = new[]
 // .WithName("GetWeatherForecast")
 // .WithOpenApi();
 app.MapControllers();
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+// var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+// var userManager = services.GetRequiredService<UserManager<AppUser>>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    // await identityContext.Database.MigrateAsync();
+    // await StoreContextSeed.SeedAsync(context);
+    // await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "An error occured during migration");
+}
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
