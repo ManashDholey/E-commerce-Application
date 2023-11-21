8b8d9bcd7c25848a296c92183317b19dd0e8c21f
Manash <Manash@DESKTOP-H7BTIBJ>
2023-11-21 22:49:40 +0530

    commit for buind product

--- Client/ecommerceClint/src/app/app.component.ts
+++ Client/ecommerceClint/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Pagination } from './models/Pagination';
+import { Product } from './models/product';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ecommerceClint';
+export class AppComponent implements OnInit{
+  
+  title = 'Ecommerce Shop';
+  products : any[] =[];
+  PageIndex=1;
+  PageSize=10;
+  Sort="priceAsc";
+  Search="Core";
+constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    try{
+    this.http.get<Pagination<Product[]>>(`https://localhost:5001/api/Products?PageIndex=${this.PageIndex}&PageSize=${this.PageSize}&Sort=${this.Sort}&Search=${this.Search}`).subscribe({
+      next: response =>{ console.log(response);
+      this.products=response.data},
+      error: error=> console.log(error),
+      complete:()=>{
+        console.log('request completed');
+        console.log('extra statement.');
+      }
+    })
+  }catch(e){
+console.log(e);
+  }
+  }
 }
