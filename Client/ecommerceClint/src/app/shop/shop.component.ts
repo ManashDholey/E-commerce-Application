5ecab4c71d3c6ab284f64e1f6932270a9e123eb9
Manash <Manash@DESKTOP-H7BTIBJ>
2023-11-24 23:59:03 +0530

    commit for add filter

--- Client/ecommerceClint/src/app/shop/shop.component.ts
+++ Client/ecommerceClint/src/app/shop/shop.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShopService } from './shop.service';
+import { Product } from '../shared/models/product';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
+import { ShopParams } from '../shared/models/ShopParams';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.scss']
 })
-export class ShopComponent {
-
+export class ShopComponent implements OnInit {
+  products : Product[] = [];
+  brands: Brand[] = [];
+  types:Type[] = [];
+  shopParams: ShopParams;
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to high', value: 'priceAsc'},
+    {name: 'Price: High to low', value: 'priceDesc'},
+  ];
+  totalCount = 0;
+  PageIndex=1;
+PageSize=10;
+Sort="priceAsc";
+Search="Core";
+brandIdSelected=0;
+typeIdSelected=0;
+  constructor(private shopService: ShopService){this.shopParams = shopService.getShopParams();}
+  ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+  getProducts() {
+    // this.shopParams.pageNumber=this.PageIndex;
+    // this.shopParams.pageSize=this.PageSize;
+    // this.shopParams.sort=this.Sort;
+     this.shopParams.search=this.Search; 
+    this.shopService.getProducts(this.brandIdSelected,this.typeIdSelected).subscribe({
+      next: (response) => { this.products = response.data.flat();
+        console.log("this.products===>",this.products);},
+      error: error => console.log(error),
+    })
+  }
+  onSortSelected(event: any) {
+    const params = this.shopService.getShopParams();
+    params.sort = event.target.value;
+    this.shopService.setShopParams(params);
+    this.shopParams = params;
+    this.getProducts();
+  }
+  getBrands() {
+    this.shopService.getBrands().subscribe({
+      next: response => this.brands = [{id:0,name:'All'},...response],
+      error: error => console.log(error),
+    })
+  }
+  getTypes() {
+    this.shopService.getTypes().subscribe({
+      next:(response :Type[])  => this.types = [{id:0,name:'All'},...response],
+      error: (error :any)=> console.log(error),
+    })
+  }
+  onBrandSelected(brandId:number){
+    this.brandIdSelected=brandId;
+    this.getProducts();
+  }
+  onTypeSelected(typeId:number){
+    this.typeIdSelected=typeId;
+    this.getProducts();
+  }
+  
 }
--- Client/ecommerceClint/src/app/shop/shop.service.ts
+++ Client/ecommerceClint/src/app/shop/shop.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../shared/models/product';
+import { Pagination } from '../shared/models/Pagination';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
+import { ShopParams } from '../shared/models/ShopParams';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+baseUrl='https://localhost:5001/api/';
 
-  constructor() { }
+shopParams = new ShopParams();
+  constructor(private http: HttpClient) { }
+
+  getProducts(brandId?:number,typeId?:number) {
+    let params = new HttpParams();
+    if (this.shopParams.brandId > 0) params = params.append('brandId', this.shopParams.brandId);
+    if (this.shopParams.typeId) params = params.append('typeId', this.shopParams.typeId);
+    params = params.append('sort', this.shopParams.sort);
+    params = params.append('pageIndex', this.shopParams.pageNumber);
+    params = params.append('pageSize', this.shopParams.pageSize);
+    if (this.shopParams.search) params = params.append('Search', this.shopParams.search);
+    console.log("params=>",params.toString());
+    return this.http.get<Pagination<Product[]>>(this.baseUrl+'products',{params});
+  }
+  getBrands() {
+    return this.http.get<Brand[]>(this.baseUrl+'products/brands');
+  }
+  getTypes() {
+    return this.http.get<Type[]>(this.baseUrl+'products/types');
+  }
+  setShopParams(params: ShopParams) {
+    this.shopParams = params;
+  }
+  getShopParams() {
+    return this.shopParams;
+  }
+  
 }
