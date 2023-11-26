bd1b3b3ec9a3ebe8005e062ba872ea95478c021b
Manash <Manash@DESKTOP-H7BTIBJ>
2023-11-26 23:37:56 +0530

    commt for add error handeler and other changes

--- Client/ecommerceClint/src/app/core/core.module.ts
+++ Client/ecommerceClint/src/app/core/core.module.ts
@@ -2,17 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
+//import { NgxSpinnerModule } from 'ngx-spinner';
 
 
 
 @NgModule({
-  declarations: [ NavBarComponent],
+  declarations: [ NavBarComponent, TestErrorComponent, NotFoundComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ToastrModule,
+    BreadcrumbModule,
+   // NgxSpinnerModule
   ],
   exports:[
-    NavBarComponent
+    NavBarComponent,
+    TestErrorComponent,
+    NotFoundComponent,
+    SectionHeaderComponent,
+   // NgxSpinnerModule
   ]
 })
 export class CoreModule { }
